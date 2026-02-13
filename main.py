03ed3f46a07d5c5f3b3130dc3e7628548d494d5a
keerthana344 <keerthiyogesh771@gmail.com>
2026-02-13 14:53:33 +0530

    deploy

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from db import get_db,DATABASE_URL
-from sqlalchemy import create_engine
+from db import get_db, engine
 from models import Base
-import os
-app = FastAPI()
+from contextlib import asynccontextmanager
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    Base.metadata.create_all(bind=engine)
+    yield
+
+app = FastAPI(lifespan=lifespan)
 
 #cors
-from fastapi.middleware.cors import CORSMiddleware
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -17,15 +21,9 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-engine=create_engine(DATABASE_URL)
-Base.metadata.create_all(engine)
-
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
-
-
-
 
 if __name__ == "__main__":
     import uvicorn
